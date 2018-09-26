487e9c5946e1d86caf0c6fb7e275a0357263882c
root <root@lent.ink>
2018-09-26 00:00:36 +0000

    2018-09-26

---
+++ html/password-reset.php
@@ -0,0 +1,7 @@
+<?php
+# https://vedovini.net/2013/05/how-to-change-your-wordpress-password-when-you-lost-all-hopes/
+define('WP_USE_THEMES', false);
+require('wp-load.php');
+#wp_set_password('', 1);
+?>
+
