e7f277b0222a194ce92834b77312a956051b85c7
svlentink <sv@lent.ink>
2018-09-30 12:41:50 +0000

    clean up

--- lisanne.lent.ink/wp-config.php
+++ lisanne.lent.ink/wp-config.php
@@ -1,6 +1,12 @@
-<?php $table_prefix = 'wp_';
-#if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') $_SERVER['HTTPS'] = 'on';
+<?php
+define('DB_NAME', 'wordpress'); #getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', 'wordpress'); #getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', 'wpdb:3306'); #getenv('WORDPRESS_DB_HOST'));
+
+$table_prefix = 'wp_li_';
 $_SERVER['HTTPS'] = 'on';
 define('WP_HOME','https://lisanne.lent.ink');
 define('WP_SITEURL','https://lisanne.lent.ink');
+#define( 'WP_DEBUG', true );
 require_once(ABSPATH . 'wp-settings.php');
--- lisanne.lent.ink/wp-content/themes/chic-lifestyle/template-parts/home-sections/newsletter-section.php~HEAD
+++
@@ -1,5 +0,0 @@
-<?php if( is_active_sidebar( 'newsletter-sidebar' ) ) { ?>
-	<div class="widget-newsletter clearfix">
-		<?php dynamic_sidebar( 'newsletter-sidebar' ); ?>
-	</div>
-<?php }
