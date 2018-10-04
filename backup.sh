030059ce4d5369c538acd51151d17e294f04c765
svlentink <sv@lent.ink>
2018-10-04 14:55:39 +0000

    2018-10-04

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 set -e
 
 cd /root/wordpress-backup
+docker exec -it mywebsite_wpdb_1 sh -c 'mysqldump --all-databases -p$MYSQL_ROOT_PASSWORD' \
+  > ~/wordpress-backup/dump.sql
 if [[ -n "$(git status -s)" ]]; then
   git add .
   git commit -m `date +%F`
