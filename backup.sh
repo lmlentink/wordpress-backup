e3bb9b20f6d605550331f4a09ba0e9b399787b25
SanderLentink.nl <sander@lent.ink>
2020-11-01 15:16:30 +0100

    2020-11-01

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
-cd /root/wordpress-backup
-docker exec -it mywebsite_wpdb_1 sh -c 'mysqldump --all-databases -p$MYSQL_ROOT_PASSWORD' \
-  > ~/wordpress-backup/dump.sql
+cd `dirname $0`
+
+if which docker >/dev/null; then
+  docker exec -it mywebsite_wpdb_1 sh -c 'mysqldump --all-databases -p$MYSQL_ROOT_PASSWORD' \
+    > dump.sql
+else
+  echo Not doing db backup, we assume it is done in kubernetes cronjob
+  echo 'https://github.com/svlentink/myinfra/blob/master/namespaces/wordpress/job.yml'
+fi
+
 if [[ -n "$(git status -s)" ]]; then
   git add .
   git commit -m `date +%F`
